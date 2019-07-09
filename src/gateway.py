f5a656d696e0ccc50fd8544d8df7d4aa7b44bdde
Daniel Devine <daniel@djdevine.co.uk>
2019-07-09 21:28:08 +0100

    Workshop Version
    7 Colours

--- src/gateway.py
+++ src/gateway.py
@@ -7,46 +7,59 @@ keyboard = Controller()
 
 SEND_RATE = 1.0
 
+pink = "p"
+yellow = "y"
+blue = "b"
+red = "r"
+green = "g"
+violet = "v"
+white = "w"
+
 def process_incoming(msg):
     print("rx:%s" % str(msg))
-    if str(msg) == "MB1:A":
-        keyboard.press(Key.left)
-        time.sleep(0.2)
-        keyboard.release(Key.left)
-        print("left")
-    elif str(msg) == "MB1:B":
-        keyboard.press(Key.right)
-        time.sleep(0.2)
-        keyboard.release(Key.right)
-        print("right")
-    elif str(msg) == "MB1:S":
-        keyboard.press(Key.up)
-        time.sleep(0.3)
-        keyboard.release(Key.up)
-        print("up")
-    #Add your incoming message processing here
-    #any radio message sent by any microbit, will arrive here
-
-def process_outgoing(msg):
-    # Example of sending a message on a timer
-    # Just call microbit.send_message whenever you have new data
-    # this will be sent to the gateway micro:bit which will then
-    # broadcast it to all listening micro:bit devices
-    print("tx:%s" % str(msg))
-    microbit.send_message(msg)
-
-next_send = time.time() + SEND_RATE
+    message = msg[0]
+    if message == pink:
+        keyboard.press('p')
+        time.sleep(0.1)
+        keyboard.release('p')
+        print("p")
+    elif message == blue:
+        keyboard.press('b')
+        time.sleep(0.1)
+        keyboard.release('b')
+        print("b")
+    elif message == red:
+        keyboard.press('r')
+        time.sleep(0.1)
+        keyboard.release('r')
+        print("c")
+    elif message == green:
+        keyboard.press('g')
+        time.sleep(0.1)
+        keyboard.release('g')
+        print("g")
+    elif message == violet:
+        keyboard.press('v')
+        time.sleep(0.1)
+        keyboard.release('v')
+        print("v")
+    elif message == white:
+        keyboard.press('w')
+        time.sleep(0.1)
+        keyboard.release('w')
+        print("w")
+    elif message == yellow:
+        keyboard.press('y')
+        time.sleep(0.1)
+        keyboard.release('y')
+        print("y")
+    else:
+        print("error")
 
 print("gateway running")
+microbit.send_message("ready\n")
 
-count = 0
 while True:
     msg = microbit.get_next_message()
     if msg is not None:
         process_incoming(msg)
-
-    #now = time.time()
-    #if now >= next_send:
-    #   next_send = now + SEND_RATE
-    #   process_outgoing(str(count))
-    #   count = (count + 1) % 10
